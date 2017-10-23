f3465616e3da0127b7a47988e76729487d6630fa
crawforda <31389347+crawforda@users.noreply.github.com>
2017-10-23 16:10:48 -0400

    Add files via upload

---
+++ lab 6.js
@@ -0,0 +1,44 @@
+function checkEmail(email) {
+    var re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.
+        [0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return re.test(email);
+
+}
+
+function checkAge() {
+    var regEx = new RegExp('\w');
+
+    if() {
+
+    } else {
+
+    }
+}
+
+function checkName() {
+
+    Function Age() {
+        validate()
+    }
+}
+form class="subform"  method="post" action="submit.html">
+    <p>
+    <label for="name" class="label">What is your name?</label>
+<input type="text" name="name" id="name">
+    </p>
+    <p>
+    RegExp() CheckName
+validate()
+    funcation myAJAXcll() {
+    alart ("I made my AJAx call")
+    alert()
+}
+Document.GetElementId
+
+var inputs= documents.getElememtByClassName('input');
+checkEmail()
+
+    <label for="email"class="label">>What is your email address?</label>
+<input type="text" name="email" id="email">
+    </p>
+
